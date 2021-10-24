db16273300cee57896fdb93828f56da6e1c57261
jake <ketchumjake@gmail.com>
2021-10-24 09:33:32 -0500

    ironically enough, added git install

--- install_programs.sh
+++ install_programs.sh
@@ -1,2 +1,6 @@
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo apt install ./teamviewer_amd64.deb
+sudo apt install vim-gtk
+sudo ./arduino-1.8.13/install.sh
+sudo apt install spacenavd
+sudo apt install git
