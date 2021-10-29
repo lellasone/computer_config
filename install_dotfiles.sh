8b2ee17ba6f6418001231932d834ed9d78ec5b7e
jake <ketchumjake@gmail.com>
2021-10-29 13:13:04 -0500

    fixed dotfile import woes

---
+++ install_dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# This is copied from / inspired by maxwell-daemons install script. 
+# his is much prettier though. 
+
+# TODO: Actually install gigavolt colors
+COLORSCHEME=${1:-gigavolt}
+
+BASE_DIR=$(dirname $(realpath $0))
+DOTFILES_DIR=$BASE_DIR/dotfiles
+SCRIPT_DIR=$BASE_DIR/bin
+SUBMODULE_DIR=$BASE_DIR/submodules
+
+# Install Vim Configs
+ln -sf $DOTFILES_DIR/vimrc $HOME/.vimrc
+
+# Install bash configs
+echo "" >> /.bashrc
+echo "# Include jake's custom .bashrc improvements" >> ~/.bashrc
+echo "if [ -f $DOTFILES_DIR/bashrc ]; then" >> ~/.bashrc
+echo "  . $DOTFILES_DIR/bashrc" >> ~/.bashrc
+echo "  echo \"Customization Loaded\"" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
+
+
+echo $DOTFILES_DIR
+echo Done!
