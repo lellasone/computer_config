8a2c4cfbc74591f5957b3fa67758b62860bba893
jake <ketchumjake@gmail.com>
2021-10-29 13:11:46 -0500

    organized and added new tools

--- install_programs.sh
+++ install_programs.sh
@@ -1,6 +1,20 @@
+# Install Remote Access Tools
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo apt install ./teamviewer_amd64.deb
-sudo apt install vim-gtk
-sudo ./arduino-1.8.13/install.sh
-sudo apt install spacenavd
+
+# Install Core Code Tools.
 sudo apt install git
+sudo apt install vim-gtk
+sudo apt install python3-pip
+sudo apt install jupyter-notebook
+
+# Install Home/School work tools. 
+sudo apt install spacenavd
+sudo apt install simplescreenrecorder
+sudo apt install inkscape
+
+# Install Project / Fabrication Tools. 
+sudo ./arduino-1.8.13/install.sh
+sudo apt install cura
+
+
